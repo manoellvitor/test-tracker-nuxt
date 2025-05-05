d4b1428e386a777d8bfcd91c4f68bf9f8c0d052a
Manoel Lopes <manoelvitorka@gmail.com>
2025-05-05 14:51:47 +0100

    :sparkles: setup tailwind / shadcn ui / icons

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100 gap-4">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div>
+      <Button class="flex items-center justify-center gap-2">
+        <Icon name="tabler:a-b" size="24" />
+        Click me
+      </Button>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "shadcn-nuxt", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
   },
 });
