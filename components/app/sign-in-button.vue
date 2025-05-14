755e7ef33ba7e4e55152c451d6652001e3afdf03
Manoel Lopes <manoelvitorka@gmail.com>
2025-05-14 22:16:20 +0100

    add dark theme support and toogle theme button (#52)

---
+++ components/app/sign-in-button.vue
@@ -0,0 +1,6 @@
+<template>
+  <button class="btn btn-outline btn-success">
+    Sign in
+    <Icon name="tabler:login-2" size="20" />
+  </button>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "shadcn-nuxt", "@nuxt/icon", "@nuxt/image"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+    "@nuxt/fonts",
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
   },
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-  shadcn: {
-    prefix: "",
-    componentDir: "./components/ui",
+  colorMode: {
+    dataValue: "theme",
   },
 });
