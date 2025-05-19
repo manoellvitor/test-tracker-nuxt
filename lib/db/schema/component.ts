bdcddfc286859b2392b9cc42ced440dfab4c1f7c
Manoel Lopes <manoelvitorka@gmail.com>
2025-05-19 19:31:34 +0100

    add better auth support/schema and configuration

--- lib/db/schema/component.ts
+++ lib/db/schema/component.ts
@@ -1,8 +1,11 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const component = sqliteTable("component", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
+  userId: int().notNull().references(() => user.id),
   serialNumber: text().notNull().unique(),
   macAddress: text().notNull().unique(),
   createdAt: int().notNull().$default(() => Date.now()),
--- lib/db/schema/index.ts
+++ lib/db/schema/index.ts
@@ -1,3 +1,4 @@
+export * from "./auth";
 export * from "./component";
 export * from "./component-image";
 export * from "./test-result";
